31d0586b7a1fc273dc01cb9cba8191ec4d4ffe22
tophua <tophua@gmail.com>
2016-10-08 16:47:27 +0800

    注释MQTTInputDStream例子

--- external/mqtt/src/main/scala/org/apache/spark/streaming/mqtt/MQTTInputDStream.scala
+++ external/mqtt/src/main/scala/org/apache/spark/streaming/mqtt/MQTTInputDStream.scala
@@ -39,14 +39,15 @@ import org.apache.spark.streaming.receiver.Receiver
 private[streaming]
 class MQTTInputDStream(
     @transient ssc_ : StreamingContext,
-    brokerUrl: String,
-    topic: String,
-    storageLevel: StorageLevel
+    brokerUrl: String,//MQTT服务器提供的消息消费地址
+    topic: String,//订阅主题名称
+    storageLevel: StorageLevel//RDD存储级别
   ) extends ReceiverInputDStream[String](ssc_) {
 
   private[streaming] override def name: String = s"MQTT stream [$id]"
 
   def getReceiver(): Receiver[String] = {
+    
     new MQTTReceiver(brokerUrl, topic, storageLevel)
   }
 }
@@ -65,16 +66,21 @@ class MQTTReceiver(
   def onStart() {
 
     // Set up persistence for messages
+    //负责对消息的持久化
     val persistence = new MemoryPersistence()
 
     // Initializing Mqtt Client specifying brokerUrl, clientID and MqttClientPersistance
+    //初始化MQTT客户端指定brokerurl,ClientID和MqttClient持久化
     val client = new MqttClient(brokerUrl, MqttClient.generateClientId(), persistence)
 
     // Callback automatically triggers as and when new message arrives on specified topic
+    //用于消息到达时自动将消息存储到Spark内存
     val callback = new MqttCallback() {
 
       // Handles Mqtt message
+      //处理Mqtt消息
       override def messageArrived(topic: String, message: MqttMessage) {
+        //将消息存储到Spark内存,getPayload获取文件字节数组
         store(new String(message.getPayload(), "utf-8"))
       }
 
@@ -82,18 +88,21 @@ class MQTTReceiver(
       }
 
       override def connectionLost(cause: Throwable) {
-        restart("Connection lost ", cause)
+        restart("Connection lost ", cause)//连接丢失
       }
     }
 
     // Set up callback for MqttClient. This needs to happen before
     // connecting or subscribing, otherwise messages may be lost
+    //设置回调mqttclient,这需要连接或订阅消息之前发生,否则可能会丢失
     client.setCallback(callback)
 
     // Connect to MqttBroker
+    //用于连接MQTT服务器
     client.connect()
 
     // Subscribe to Mqtt topic
+    //用于订阅消息主题
     client.subscribe(topic)
 
   }
