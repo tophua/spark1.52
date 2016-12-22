578b27435006186202e50cff1481733462153841
tophua <tophua@gmail.com>
2016-12-22 13:44:31 +0800

    ReceiverInputDStreamSuite.scala

--- streaming/src/test/scala/org/apache/spark/streaming/ReceiverInputDStreamSuite.scala
+++ streaming/src/test/scala/org/apache/spark/streaming/ReceiverInputDStreamSuite.scala
@@ -52,25 +52,41 @@ class ReceiverInputDStreamSuite extends TestSuiteBase with BeforeAndAfterAll {
   testWithoutWAL("createBlockRDD creates empty BlockRDD when no block info") { receiverStream =>
     val rdd = receiverStream.createBlockRDD(Time(0), Seq.empty)
     assert(rdd.isInstanceOf[BlockRDD[_]])
+    //判断是否能转换预写日志块
+    val isInstance=rdd.isInstanceOf[WriteAheadLogBackedBlockRDD[_]]
+    //println("======"+isInstance)
     assert(!rdd.isInstanceOf[WriteAheadLogBackedBlockRDD[_]])
     assert(rdd.isEmpty())
   }
   //创建块的信息正确blockrdd WAL(预写式日志)
   testWithoutWAL("createBlockRDD creates correct BlockRDD with block info") { receiverStream =>
     val blockInfos = Seq.fill(5) { createBlockInfo(withWALInfo = false) }
-    val blockIds = blockInfos.map(_.blockId)
+    //List(input-0--2296022686498889608,input-0-8033641072805079561,input-0-4856295788039827092,input-0--5257505035776562917,input-0-252513990646127979)
+    val blockIds = blockInfos.map(x=>{
+        println("======"+x.blockId)      
+        x.blockId
+              })
 
-    // Verify that there are some blocks that are present, and some that are not
-    //确认有一些块是存在的,有些是不存在的
+    //Verify that there are some blocks that are present, and some that are not
+    //确认有一些块是否存在
     require(blockIds.forall(blockId => SparkEnv.get.blockManager.master.contains(blockId)))
-
+    //创建一个BlockRDD
     val rdd = receiverStream.createBlockRDD(Time(0), blockInfos)
     assert(rdd.isInstanceOf[BlockRDD[_]])
+    //判断没有预写日志BlockRDD
     assert(!rdd.isInstanceOf[WriteAheadLogBackedBlockRDD[_]])
     val blockRDD = rdd.asInstanceOf[BlockRDD[_]]
+    /**
+     * blockIds=
+     * ======input-0--6065065102381848782
+     *  ======input-0--2488062752272461646
+     *  ======input-0-6777333610868239819
+     *  ======input-0-1311392156326201126
+     *  ======input-0-7149492482940956743
+     */
     assert(blockRDD.blockIds.toSeq === blockIds)
   }
-  //创建blockrdd块滤波不存在WAL(预写式日志)
+  //创建blockrdd块过虑不存在WAL(预写式日志)
   testWithoutWAL("createBlockRDD filters non-existent blocks before creating BlockRDD") {
     receiverStream =>
       val presentBlockInfos = Seq.fill(2)(createBlockInfo(withWALInfo = false, createBlock = true))
@@ -79,7 +95,7 @@ class ReceiverInputDStreamSuite extends TestSuiteBase with BeforeAndAfterAll {
       val blockIds = blockInfos.map(_.blockId)
 
       // Verify that there are some blocks that are present, and some that are not
-      //确认有一些块是存在的,有些是不存在的
+      //确认一些块是否存在
       require(blockIds.exists(blockId => SparkEnv.get.blockManager.master.contains(blockId)))
       require(blockIds.exists(blockId => !SparkEnv.get.blockManager.master.contains(blockId)))
 
@@ -105,6 +121,8 @@ class ReceiverInputDStreamSuite extends TestSuiteBase with BeforeAndAfterAll {
       assert(rdd.isInstanceOf[WriteAheadLogBackedBlockRDD[_]])
       val blockRDD = rdd.asInstanceOf[WriteAheadLogBackedBlockRDD[_]]
       assert(blockRDD.blockIds.toSeq === blockIds)
+     // val seq=blockRDD.walRecordHandles.toSeq
+      //println("==="+seq.mkString(","))
       assert(blockRDD.walRecordHandles.toSeq === blockInfos.map { _.walRecordHandleOption.get })
   }
   //创建blockrdd当某块信息没有WAL信息 WAL(预写式日志)
@@ -137,7 +155,9 @@ class ReceiverInputDStreamSuite extends TestSuiteBase with BeforeAndAfterAll {
   private def runTest(enableWAL: Boolean, body: ReceiverInputDStream[_] => Unit): Unit = {
     val conf = new SparkConf()
     conf.setMaster("local[4]").setAppName("ReceiverInputDStreamSuite")
+    //设置预写式日志是否可用
     conf.set(WriteAheadLogUtils.RECEIVER_WAL_ENABLE_CONF_KEY, enableWAL.toString)
+    //判断预写日志是否可用的状态
     require(WriteAheadLogUtils.enableReceiverLog(conf) === enableWAL)
     val ssc = new StreamingContext(conf, Seconds(1))
     val receiverStream = new ReceiverInputDStream[Int](ssc) {
@@ -160,13 +180,17 @@ class ReceiverInputDStreamSuite extends TestSuiteBase with BeforeAndAfterAll {
       withWALInfo: Boolean,
       createBlock: Boolean = true): ReceivedBlockInfo = {
     val blockId = new StreamBlockId(0, Random.nextLong())
-    if (createBlock) {
+    if (createBlock) {//判断是否创建块
+      //存储块,是否调用master
       SparkEnv.get.blockManager.putSingle(blockId, 1, StorageLevel.MEMORY_ONLY, tellMaster = true)
+      //判断master是否包含blockId
       require(SparkEnv.get.blockManager.master.contains(blockId))
     }
-    val storeResult = if (withWALInfo) {
+    val storeResult = if (withWALInfo) {//预写日志信息
+      //预写日志记录存储
       new WriteAheadLogBasedStoreResult(blockId, None, new WriteAheadLogRecordHandle { })
     } else {
+      //管理块记录存储
       new BlockManagerBasedStoreResult(blockId, None)
     }
     new ReceivedBlockInfo(0, None, None, storeResult)
