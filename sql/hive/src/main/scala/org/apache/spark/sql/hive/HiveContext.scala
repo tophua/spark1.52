8e620209b4e2d2a1152a660b9eb0747b7713e082
tophua <tophua@gmail.com>
2017-06-28 10:25:59 +0800

    build eror

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
@@ -106,8 +106,7 @@ class HiveContext(sc: SparkContext) extends SQLContext(sc) with Logging {
    *   - The CTAS statement specifies SequenceFile (STORED AS SEQUENCEFILE) as the file format
    *     and no SerDe is specified (no ROW FORMAT SERDE clause).
    */
-  protected[sql] def convertCTAS: Boolean = getConf(CONVE
-    RT_CTAS)
+  protected[sql] def convertCTAS: Boolean = getConf(CONVERT_CTAS)
 
   /**
    * The version of the hive client that will be used to communicate with the metastore.  Note that
