6bee4d44e5ad2c129a43ab26a0a1cbf56299757f
tophua <tophua@gmail.com>
2017-07-14 13:53:01 +0800

    把Scala List的几种常见方法梳理汇总如下，日常开发场景基本上够用了。

--- examples/src/main/scala/org/apache/spark/examples/sql/hive/HiveFromSpark.scala
+++ examples/src/main/scala/org/apache/spark/examples/sql/hive/HiveFromSpark.scala
@@ -33,15 +33,19 @@ object HiveFromSpark {
     sql(s"LOAD DATA LOCAL INPATH '${kv1File.getAbsolutePath}' INTO TABLE src")
 
     // Queries are expressed in HiveQL
+    //查询在HiveQL中表达
     println("Result of 'SELECT *': ")
     sql("SELECT * FROM src").collect().foreach(println)
 
     // Aggregation queries are also supported.
+    //还支持聚合查询
     val count = sql("SELECT COUNT(*) FROM src").collect().head.getLong(0)
     println(s"COUNT(*): $count")
 
     // The results of SQL queries are themselves RDDs and support all normal RDD functions.  The
     // items in the RDD are of type Row, which allows you to access each column by ordinal.
+    //SQL查询的结果本身就是RDD，并支持所有正常的RDD功能,
+    //RDD中的项目的类型为Row，它允许您按顺序访问每个列。
     val rddFromSql = sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
 
     println("Result of RDD.map:")
@@ -50,10 +54,12 @@ object HiveFromSpark {
     }
 
     // You can also register RDDs as temporary tables within a HiveContext.
+    //您还可以将RDD注册为HiveContext中的临时表。
     val rdd = sc.parallelize((1 to 100).map(i => Record(i, s"val_$i")))
     rdd.toDF().registerTempTable("records")
 
     // Queries can then join RDD data with data stored in Hive.
+    //然后，查询可以使用Hive中存储的数据加入RDD数据。
     println("Result of SELECT *:")
     sql("SELECT * FROM records r JOIN src s ON r.key = s.key").collect().foreach(println)
 
