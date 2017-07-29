d04ce9bfaebf039a696455a1f0ca25257d520905
tophua <tophua@gmail.com>
2017-07-29 17:09:23 +0800

    http://www.cnblogs.com/wanqieddy/p/3853863.html ExecutorService
    获取任务的执行的返回值
    在Java5之后，任务分两类：一类是实现了Runnable接口的类，一类是实现了Callable接口的类。两者都可以被ExecutorService执行，但是Runnable任务没有返回值，而Callable任务有返回值。并且Callable的call()方法只能通过ExecutorService的(<T> task) 方法来执行，并且返回一个 <T><T>，是表示任务等待完成的 Future。

--- core/src/main/scala/org/apache/spark/HeartbeatReceiver.scala
+++ core/src/main/scala/org/apache/spark/HeartbeatReceiver.scala
@@ -32,6 +32,8 @@ import org.apache.spark.util.{Clock, SystemClock, ThreadUtils, Utils}
  * A heartbeat from executors to the driver. This is a shared message used by several internal
  * components to convey liveness or execution information for in-progress tasks. It will also
  * expire the hosts that have not heartbeated for more than spark.network.timeout.
+  * 从执行者到driver的心跳, 这是几个内部组件使用的共享消息,用于传达正在进行的任务的活动或执行信息。
+  * 它也将使不超过spark.network.timeout的心跳的主机过期。
  */
 private[spark] case class Heartbeat(
     executorId: String,
@@ -41,6 +43,7 @@ private[spark] case class Heartbeat(
 /**
  * An event that SparkContext uses to notify HeartbeatReceiver that SparkContext.taskScheduler is
  * created.
+  * SparkContext用于通知HeartbeatReceiver SparkContext.taskScheduler的事件。
  */
 private[spark] case object TaskSchedulerIsSet
 
@@ -54,6 +57,7 @@ private[spark] case class HeartbeatResponse(reregisterBlockManager: Boolean)
 
 /**
  * Lives in the driver to receive heartbeats from executors..
+  * driver活着得到执行者的心跳
  */
 private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
   extends ThreadSafeRpcEndpoint with SparkListener with Logging {
@@ -93,6 +97,7 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
 
   // "eventLoopThread" is used to run some pretty fast actions. The actions running in it should not
   // block the thread for a long time.
+  //“eventLoopThread”用于运行一些非常快的动作,其中运行的操作不应该长时间堵塞线程。
   private val eventLoopThread =
     ThreadUtils.newDaemonSingleThreadScheduledExecutor("heartbeat-receiver-event-loop-thread")
 
@@ -150,6 +155,8 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
           // after we just removed it. It's not really an error condition so we should
           // not log warning here. Otherwise there may be a lot of noise especially if
           // we explicitly remove executors (SPARK-4134).
+          //如果我们刚刚删除后立即得到执行者的飞行心跳，可能会发生这种情况,这不是真的错误的条件,所以我们不应该在这里记录警告。
+          // 否则可能会有很多噪音,特别是如果我们明确删除执行器（SPARK-4134）。
           logDebug(s"Received heartbeat from unknown executor $executorId")
           context.reply(HeartbeatResponse(reregisterBlockManager = true))
         }
@@ -157,6 +164,7 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
         // Because Executor will sleep several seconds before sending the first "Heartbeat", this
         // case rarely happens. However, if it really happens, log it and ask the executor to
         // register itself again.
+        //因为执行者将在发送第一个“心跳”之前睡眠几秒钟,这种情况很少发生,但是,如果真的发生了,请登录并请执行人再次注册。
         logWarning(s"Dropping $heartbeat because TaskScheduler is not ready yet")
         context.reply(HeartbeatResponse(reregisterBlockManager = true))
       }
@@ -168,6 +176,7 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
    *
    * @return if HeartbeatReceiver is stopped, return None. Otherwise, return a Some(Future) that
    *         indicate if this operation is successful.
+    *         如果HeartbeatReceiver被停止,则返回None,否则,返回一个(Future),指示此操作是否成功。
    */
   def addExecutor(executorId: String): Option[Future[Boolean]] = {
     //向自己发送ExecutorRegistered消息,receiveAndReply方法接收消息
@@ -188,6 +197,7 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
    *
    * @return if HeartbeatReceiver is stopped, return None. Otherwise, return a Some(Future) that
    *         indicate if this operation is successful.
+    *         如果HeartbeatReceiver被停止,则返回None,否则,返回一个(Future),指示此操作是否成功。
    */
   def removeExecutor(executorId: String): Option[Future[Boolean]] = {
     Option(self).map(_.ask[Boolean](ExecutorRemoved(executorId)))
@@ -195,13 +205,17 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, clock: Clock)
 
   /**
    * If the heartbeat receiver is not stopped, notify it of executor removals so it doesn't
-   * log superfluous errors.如果心跳接收器没有停止,请通知它的执行器删除,否则不会记录多余的错误
+   * log superfluous errors.
+    * 如果心跳接收器没有停止,请通知它的执行器删除,否则不会记录多余的错误
    *
    * Note that we must do this after the executor is actually removed to guard against the
    * following race condition: if we remove an executor's metadata from our data structure
    * prematurely, we may get an in-flight heartbeat from the executor before the executor is
    * actually removed, in which case we will still mark the executor as a dead host later
    * and expire it with loud error messages.
+    * 请注意:我们必须在执行者被实际删除以防止以下竞争条件之后执行此操作：
+    * 如果我们过早地从数据结构中删除了执行者的元数据,则在执行者实际删除之前,我们可能会从执行器获取一个正在运行中的心跳,
+    * 在这种情况下,我们仍然会将执行人标记为死主,稍后会出现大声错误讯息。
    */
   override def onExecutorRemoved(executorRemoved: SparkListenerExecutorRemoved): Unit = {
     removeExecutor(executorRemoved.executorId)
